6ee15c01081ada07adc7b8b934258e16ccaf795e
sisa213 <sisaruizm@gmx.com>
2022-10-20 17:45:46 +0200

    Replaced fetch with axios + comments

--- setupTables.mjs
+++ setupTables.mjs
@@ -1,109 +1,102 @@
-import mysql from 'mysql';
-import fetch from 'node-fetch';
-import { promisify } from 'util';
+import mysql from 'mysql'
+import axios from 'axios'
 
-const sql = "INSERT INTO probes (id, country, longitude, latitude) VALUES (?,?,?,?)";
-const sql1 = "INSERT INTO anchors (id, fqdn, country, longitude, latitude) VALUES (?,?,?,?,?)";
-const PAGE_SIZE = 100;
-//let i=0, a=0;
+// sql query statements
+const sql0 = "INSERT INTO probes (id, country, longitude, latitude) VALUES (?,?,?,?)"
+const sql1 = "INSERT INTO anchors (id, fqdn, country, longitude, latitude) VALUES (?,?,?,?,?)"
+const PAGE_SIZE = 100      // number of ojects listed in a single page
+let pool     // global pool connection for queries
 
-const con = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'probes&anchors'
-});
+try {   // creating pool connection to database
+    pool = mysql.createPool({
+        connectionLimit : 5,
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'probes&anchors',
+    })
 
-con.connect((err)=>{
-    if(err){
-        console.log("Connection not proper");
-    }else{
-        console.log("connected");
-    }
-});
+} catch (err){
+    console.log(err)
+}
 
+/**
+ * getNpgs: returns number of pages of a resource in RIPE Atlas API
+ */
 const getNpgs = async (url) => {
 
-    const res = await fetch(url);
-    if (!res.ok) {
-        const message = `An error has occured: ${res.status}`;
-        throw new Error(message);
-    }
-    const data = await res.json();
-    console.log(data.count);
-    return Math.ceil(data.count/PAGE_SIZE);     
+    const res = await axios.get(url)
+    const data = await res.data
+
+    console.log("number of objects: "+data.count)
+    return Math.ceil(data.count/PAGE_SIZE)
 }
+
+/**
+ * getProbes: fetches probes from RIPE Atlas API and stores them in the database
+ */
 
 const getProbes = async (npg, stat) => {
-    console.log("Sono in probes "+stat+"\n fetching in corso ...");
-    for (let j=1; j<=npg; j++) {
-        //console.log("j="+j);
-        let URLi = "https://atlas.ripe.net/api/v2/probes/?page="+j+"&status="+stat;
-        const res = await fetch(URLi);
-        if (!res.ok) {
-            const message = `An error has occured: ${res.status}`;
-            throw new Error(message);
-        }
-        const data = await res.json();
+    console.log("I'm inside getProbes "+stat+"\n fetching ongoing ...")
+
+    for (let i=1; i<=npg; i++) {
+        let URLi = "https://atlas.ripe.net/api/v2/probes/?page="+i+"&status="+stat
+        const res = await axios.get(URLi)   //fetching probes with status equal to 'stat'
+        const data = res.data
+
         for (let probe of data.results){
-           // i++;
-            let id0 = probe.id;
+
+            // storing only probes with defined location
             if (probe.country_code!=null && probe.geometry!=null){
 
-                let country = probe.country_code;
-                let longitude = probe.geometry.coordinates[0];
-                let latitude = probe.geometry.coordinates[1];
+                let id0 = probe.id     // probe id
+                let country = probe.country_code   // probe country
+                let longitude = probe.geometry.coordinates[0]  // probe position: longitude
+                let latitude = probe.geometry.coordinates[1]   // probe position: latitude
 
-                //console.log("id0: "+id0+"\t"+"cc: "+country+"\t"+"long: "+longitude+"\t"+"lati: "+latitude);
-                //console.log(i);
-
-                let promisifydbconnection = promisify(con.query).bind(con);
-                await promisifydbconnection(sql, [id0, country, longitude, latitude]);
+                const promisePool = pool.promise()
+                // query database using promises
+                await promisePool.execute(sql0, [id0, country, longitude, latitude])
             }
-
         }
-        
     }
 }
 
+/**
+ * getAnchors: fetches anchors from RIPE Atlas API and stores them in the database
+ */
 const getAnchors = async (npg) => {
-    console.log("Sono in anchors. \n Fetching in corso ...");
-    for (let k=1; k<=npg; k++) {
-        //console.log("k="+k);
-        let URLi = "https://atlas.ripe.net/api/v2/anchors/?page="+k;
-        const res = await fetch(URLi);
-        if (!res.ok) {
-            const message = `An error has occured: ${res.status}`;
-            throw new Error(message);
-          }
-        const data = await res.json();
-        for (let anchor of data.results){
-            //a++;
-            let id0 = anchor.probe;
-            let fqdn = anchor.fqdn;
-            let country = anchor.country;
-            let longitude = anchor.geometry.coordinates[0];
-            let latitude = anchor.geometry.coordinates[1];
+    console.log("I'm inside getAnchors \n fetching ongoing ...")
+    for (let i=1; i<=npg; i++) {
+        let URLi = "https://atlas.ripe.net/api/v2/anchors/?page="+i
+        const res = await axios.get(URLi)   // fetching anchors
+        const data = res.data
 
-            let promisifydbconnection = promisify(con.query).bind(con);
-            await promisifydbconnection(sql1, [id0, fqdn, country, longitude, latitude]);
-            //console.log("id0: "+id0+"\t"+"cc: "+country+"\t"+"long: "+longitude+"\t"+"lati: "+latitude);
-            //console.log(a);
+        for (let anchor of data.results){
+
+            let id = anchor.probe  // anchor id
+            let fqdn = anchor.fqdn  // anchor fqdn
+            let country = anchor.country    // anchor country
+            let longitude = anchor.geometry.coordinates[0]  // anchor position: longitude
+            let latitude = anchor.geometry.coordinates[1]   // anchor position: latitude
+
+            const promisePool = pool.promise()
+            // query database using promises
+            await promisePool.execute(sql1, [id, fqdn, country, longitude, latitude]);
         }
-        
     }
 }
 
 async function main(){
     
     const pgs1 = await getNpgs("https://atlas.ripe.net/api/v2/probes/?status=1");
-    await getProbes(pgs1, 1);
+    await getProbes(pgs1, 1)    // storing connected probes (status=1)
     const pgs2 = await getNpgs("https://atlas.ripe.net/api/v2/probes/?status=2");
-    await getProbes(pgs2, 2);
+    await getProbes(pgs2, 2)   // storing disconnected probes (status=2)
     const pgs3 = await getNpgs("https://atlas.ripe.net/api/v2/anchors");
-    await getAnchors(pgs3);
-    con.end();
-    console.log("Fetching terminato.");
+    await getAnchors(pgs3)     // storing anchors
+    await promisePool.end()    // close pool connection
+    console.log("Fetching terminated.")
 }
 
 main();
