d6c8b9bfad31b267746178b836838f5839074d8d
sisa213 <sisaruizm@gmx.com>
2022-10-20 11:40:39 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch'
 import express from 'express'
 import mysql from 'mysql2'
 import { performance } from 'perf_hooks'
@@ -9,16 +8,15 @@ import axios from 'axios'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const port = process.env.PORT || 8080;
 
-let sources;    // probes and anchors in from_country
-let targets;    // anchors in to_country
-let values = new Array();     
-let time_span = 7;     //days
-let num_results = 0;
-let seconds0;
+let sources = [];    // probes and anchors in from_country
+let targets = [];    // anchors in to_country
+let values = [];     // results collected
+const time_span = 7;     //number of days considered
+let num_results = 0;    //number of results collected
 
 const app = express();
-
 app.engine('.html', ejs.__express);
 app.use(express.json());
 app.use(express.static(__dirname));
@@ -29,8 +27,6 @@ app.set('view engine', 'ejs');
 app.get('', (req, res) => {
     res.send('index.html');
 })
-
-const port = process.env.PORT || 8080;
 
 /*
 fillArrays: initializes sources and targets with data from probes&anchors database
@@ -81,73 +77,58 @@ function getTimestampInSeconds (now) {
 fetchData: fetches and stores in values (array) ping measurement results of interest
 */
 const fetchData = async () => {
-    console.log("Sono dentro fetchData e qui targets deve essere pronto");
-    let start_time = getTimestampInSeconds(false);
-    let anchor_source=0;
+    console.log("Sono dentro fetchData e qui targets deve essere pronto")
+    let start_time = getTimestampInSeconds(false)
+    let anchor_source=0
 
     for ( let t of targets ){
 
-        let new_page = 'https://atlas.ripe.net/api/v2/measurements/ping/?status=2&target='+t+'&description__contains=mesh&optional_fields=probes';
-        console.log(new_page);
+        let new_page = 'https://atlas.ripe.net/api/v2/measurements/ping/?status=2&target='+t+'&description__contains=anchoring&optional_fields=probes'
+        console.log(new_page)
 
-        let response = await fetch(new_page);    
-        const data = await response.json();
-        if (data.count==0) continue;
+        let response = await axios.get(new_page)
+        const data = response.data
+        if (data.count==0) continue
         let measurement = data.results[0]
-
-        console.log("sono dentro il secondo for");
 
         const prbs_list = measurement.probes.map( i =>  i.id)
 
-        const filtered_sources = sources.filter(value => prbs_list.includes(value));
-        console.log("#filtered_sources: "+filtered_sources.length+"\n"+filtered_sources);
+        const filtered_sources = sources.filter(value => prbs_list.includes(value))
+        console.log("#filtered_sources: "+filtered_sources.length+"\n"+filtered_sources)
 
         if ( Array.isArray(filtered_sources) && filtered_sources.length ){
 
-            if (anchor_source==0 || !filtered_sources.includes(anchor_source)){
-                anchor_source = filtered_sources[0]
-            }
-          
-            //aggiungo una query per lo starttime
-            const result_link = measurement.result;
-            let url_filtered_results = result_link+"?probe_ids="+anchor_source+"&start="+start_time;
+            for( let fs of filtered_sources ){
 
-            console.log(url_filtered_results);
-                
-            //const res_results = await fetch(url_filtered_results);
-            var startTime0 = performance.now();
-            const res_results = await axios.get(url_filtered_results);
-            var endTime0 = performance.now();
-            seconds0 = (endTime0-startTime0)/1000;
+                //aggiungo una query per lo starttime
+                const result_link = measurement.result;
+                let url_filtered_results = result_link+"?probe_ids="+fs+"&start="+start_time;
+                console.log(url_filtered_results);
+                const res_results = await axios.get(url_filtered_results);
 
-            /* var startTime0 = performance.now();
-            let result_set = await res_results.json();  */
-            let result_set = res_results.data 
-            /* var endTime0 = performance.now();
-            seconds0 = (endTime0-startTime0)/1000; */
+                let result_set = res_results.data
 
-            if ( Object.keys(result_set).length === 0){
-                        continue;
-            }
+                if ( Object.keys(result_set).length === 0){
+                    continue;
+                }
+                console.log("sono dentro un result_set: ..dovrebbe essere uno valido a cui poi seguono i valori presi");
 
-            console.log("sono dentro un result_set: ..dovrebbe essere uno valido a cui poi seguono i valori presi");
-
-            let base_time = start_time;
-            for (let result of result_set){ 
-                     
-                if(result.avg!=-1 && result.timestamp >= base_time){
+                let base_time = start_time;
+                for (let result of result_set){ 
+                        
+                    if(result.avg!=-1 && result.timestamp >= base_time){
                         num_results++;
                         console.log(num_results);
 
                         let n_elem = new Object();
-                        n_elem.targets = t;
+                        n_elem.pair = t+result.prb_id;
                         n_elem.timestamp = result.timestamp;
                         n_elem.RTT = result.avg;
                         values.push(n_elem);
-                        //console.log(n_elem);
-                        base_time = result.timestamp+1200;
+                        base_time = result.timestamp+1800;
+                    }
                 }
-            }
+            }             
         }
     }
 }
@@ -160,11 +141,12 @@ function compressDataset(ar){
     //sort array by timestamp
     ar.sort((a, b) => a.timestamp - b.timestamp);
 
-    //get subarrays
-    let third_hours_span = time_span*72
+    //get subarrays (48 half_hours in one day)
+    let half_hours_span = time_span*48;
     console.log("num_results: "+num_results)
-    console.log("third_hours_span: "+third_hours_span)
-    let sub_index = Math.floor(num_results / third_hours_span);
+    console.log("half_hours_span: "+half_hours_span)
+    let sub_index = Math.floor(num_results / half_hours_span);
+
     if (sub_index>=1){
 
         console.log("sub_index: "+sub_index);
@@ -200,13 +182,13 @@ function compressDataset(ar){
             obj.RTT = getAverage(sub_ar.map(p=>p.RTT))
 
             //get number of couples source-probe participating
-            const probes_set = new Set()
+            const pairs_set = new Set()
             sub_ar.forEach(element => {
-                probes_set.add(element.targets)
+                pairs_set.add(element.pair)
             });
-            console.log("number of targets for sub_ar: "+probes_set);
+            console.log("number of pairs for sub_ar: "+pairs_set);
 
-            obj.probes = probes_set.size
+            obj.pairs = pairs_set.size
 
             new_ar.push(obj);
 
@@ -219,7 +201,7 @@ function compressDataset(ar){
         return new_ar
     }
     else{
-        ar.map(elem=>{elem.probes=1})
+        ar.map(elem=>{elem.pairs=1})
         return ar
     }
 
@@ -254,11 +236,8 @@ app.post('/', async function(req,res){
                          from: regionNames.of(req.body.from_country),
                          to: regionNames.of(req.body.to_country)});
 
-    console.log("End");
-    console.log( dataset.filter(obj => {return obj.RTT>100}));
     console.log(`Call to fetchData took ${seconds} seconds`);
-    console.log(`Call to await .json() took ${seconds0} seconds`);
-    //console.log(values);
+    console.log("End");
 });
 
 
